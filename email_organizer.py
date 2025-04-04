c67343d5705054f7a70a723c724759f97b98549e
Phenriqx <pedroxlbd@gmail.com>
2025-04-04 10:07:09 -0300

    email auth for api

---
+++ email_organizer.py
@@ -0,0 +1,32 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.utils import parseaddr
+import os 
+import pickle
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+class EmailAuth:
+    def get_email_service():
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+                
+        # If no valid credentials -> authenticate user
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            
+            # Save credentials
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        return build('gmail', 'v1', credentials=creds)
+
+class 
+
+service = EmailAuth.get_email_service()
